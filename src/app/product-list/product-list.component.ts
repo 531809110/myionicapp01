7b528e91ccaeadd503937459b9c5d562dd1b7de9
531809110 <531809110@qq.com>
2019-10-03 22:41:54 +0800

    完成商品列表大部分，没有写分页

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,11 +7,14 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
+private productList:object[]=[]
   constructor(private http:HttpClient) { }
 loadList(){
-  let url="http://www.codeboy.com/data/product/list.php?pno=2&kw=dell"
-  this.http.get(url).subscribe((res:any)=>{console.log(res)})
+  let url="http://www.codeboy.com/data/product/list.php?pno=2"
+  this.http.get(url).subscribe((res:any)=>{
+    this.productList=res.data;
+    console.log(this.productList)
+  })
 }
   ngOnInit() {
     this.loadList();
