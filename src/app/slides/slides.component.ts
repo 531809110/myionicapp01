03f81d1abb24366b455fa8e1e885be7e80b44fb6
531809110 <52400589+531809110@users.noreply.github.com>
2019-10-08 14:08:02 +0800

    增加轮播图自动播放函数

--- src/app/slides/slides.component.ts
+++ src/app/slides/slides.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+
 @Component({
   selector: 'app-slides',
   templateUrl: './slides.component.html',
@@ -9,9 +10,11 @@ export class SlidesComponent implements OnInit {
   @Input()
   private slides:object[]=[];
   slideOpts = {
-    initialSlide: 1,
+    initialSlide: 0,
     speed: 400
   };
+  @ViewChild("myAd",{static:true})
+  private myAd;
   constructor(private http:HttpClient) { };
 // public loadslides():void{
 //   let url=' http://www.codeboy.com/data/product/index.php'
@@ -22,5 +25,6 @@ export class SlidesComponent implements OnInit {
 // }
   ngOnInit() {
     // this.loadslides();
+    this.myAd.startAutoplay();
   }
 }
