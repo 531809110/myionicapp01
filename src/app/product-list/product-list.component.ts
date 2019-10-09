9da00a24786dd56ee32cb64ac3fecc12b3c1ff94
531809110 <52400589+531809110@users.noreply.github.com>
2019-10-09 15:22:32 +0800

    增加搜索功能

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,12 +13,32 @@ export class ProductListComponent implements OnInit {
 private productList:object[]=[];
 // 新请求的产品列表
 private newList:object[]=[];
+//双向绑定搜索框内容
+private searchText:string;
 //加载到底部开关
 private bottomtext:boolean=false;
 @ViewChild("myScroll",{static:true})
 private infiniteScroll;
 //构造异步请求变量http
   constructor(private http:HttpClient) { }
+  // 页头返回按钮
+  jump() {
+    window.history.go(-1)
+  }
+  //定义搜索框搜索函数:当搜索框内容改变时，发送查询请求
+  search(){
+    if(this.searchText!=""){
+    let url=`http://www.codeboy.com/data/product/list.php?kw=${this.searchText}`
+  this.http.get(url).subscribe((res:any)=>{
+    this.productList=res.data;
+    // console.log(this.productList);
+  })}else if(this.searchText==""){
+    this.productList=[];
+    this.newList=[];
+    this.pno=0;
+    this.loadList();
+  }
+  }
   //定义上拉时，调用分页显示函数，并且在请求不到数据时，禁用上拉，同时显示无更多数据了
   loadData(event) {
     this.loadList();
